c46c8cff4915d6834e8b5e422b1e04564409ed6e
DockingMule <dockingmule@pm.me>
2025-12-23 16:50:18 -0500

    more specific map location

--- components/map.tsx
+++ components/map.tsx
@@ -1,16 +1,16 @@
 import { Platform, StyleSheet } from 'react-native';
 
 export default function Map() {
-  const latitude = 39.84980594477792;
-  const longitude = -75.10543198038818;
-  const zoom = 15; // Adjust this: 1 (world) to 20 (buildings)
+  // Extracted from Google Maps embed URL
+  const latitude = 39.849365711258024;
+  const longitude = -75.10678544412546;
 
   // Only import MapView on native platforms
   if (Platform.OS === 'web') {
     return (
       <iframe
         style={styles.map as any}
-        src={`https://maps.google.com/maps?q=${latitude},${longitude}&hl=en&z=${zoom}&output=embed`}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1325.6169897731074!2d-75.10678544412546!3d39.849365711258024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6ce350ce638bf%3A0xf91346f84eada73b!2sDeptford%20Sports%20Complex!5e0!3m2!1sen!2sus!4v1766526093199!5m2!1sen!2sus"
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
